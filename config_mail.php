51ef8fbcf368833e297c436b274400228d861934
huetrinh204 <trinhfokko@gmail.com>
2025-12-11 17:09:49 +0700

    Remove sensitive file

--- config_mail.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    "client_id"     => "58707631760-jdnhnoscf1fl80kbn5dk41pgm2go9ebo.apps.googleusercontent.com",
-    "client_secret" => "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",
-    "refresh_token" => "1//0g1M1aSOKfIYfCgYIARAAGBASNwF-L9IrIlp7byV-ZxmU07VibeevK0iGfbbRQc914AMTCgmgdnQDRcGxfVPTjkFmr8J4R86X174",
-    "email"         => "trinhfokko@gmail.com"
-];
